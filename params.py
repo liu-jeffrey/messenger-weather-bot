fb3f8ed6540716c244bdad96ce98c61292f4e47b
Jeffelini <jeffreyliu0117@gmail.com>
2019-11-17 17:24:22 -0500

    Build params for AccuWeather api

---
+++ params.py
@@ -0,0 +1,13 @@
+# %threshold for probability of rain or snow
+# message is only sent out if threshold is exceeded
+RAIN_THRESHOLD = 25
+SNOW_THRESHOLD = 25
+
+# time between Accuweather request (in hours)
+UPDATE_INTERVAL_HR = 1
+
+# delay time between messages (in hours)
+DELAY_TIME_HR = 4
+
+# location ID
+LOCATION_ID = "55073"
